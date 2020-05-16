ef059fbd2f24a77166a2931a6881e19f176020bd
sayedmelb <syedawakil@gmail.com>
2020-05-16 11:38:46 +1000

    4 files modified

--- src/app/navbar/nav-bar.ts
+++ src/app/navbar/nav-bar.ts
@@ -1,40 +1,34 @@
-import { Component, OnInit, ViewChild, Injectable, Output, Input, EventEmitter } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  Injectable,
+  Output,
+  Input,
+  EventEmitter
+} from "@angular/core";
 
 @Component({
-  selector: 'nav-bar',
-  templateUrl: './nav-bar.html',
-  styleUrls: ['./nav-bar.scss'],
- 
+  selector: "nav-bar",
+  templateUrl: "./nav-bar.html",
+  styleUrls: ["./nav-bar.scss"]
 })
 export class NavBar implements OnInit {
-    
-    @Output() navstatus = new EventEmitter();
-    @Input('headerInfo') headerInfo: any;
- 
+  @Output() navstatus = new EventEmitter();
+  @Input("headerInfo") headerInfo: any;
 
+  constructor() {}
 
-  constructor() {
-  
+  ngOnInit() {}
+
+  setHome(event, navitem: string) {
+    let evn = event;
+    let styleclass: string = "active";
+    const btnNor = document.getElementsByClassName(
+      styleclass
+    ) as HTMLCollectionOf<HTMLElement>;
+    btnNor[0].classList.remove(styleclass);
+    evn.currentTarget.classList.add(styleclass);
+    this.navstatus.emit({ state: navitem });
   }
-
-  ngOnInit() {
-  }
-
-  setHome(event, navitem: string){
-     
-
-     let evn = event;
-  let styleclass: string = "active";
-  const btnNor = document.getElementsByClassName(
-    styleclass
-  ) as HTMLCollectionOf<HTMLElement>;
-  btnNor[0].classList.remove(styleclass);
-  evn.currentTarget.classList.add(styleclass);
-  this.navstatus.emit({state: navitem});
-
-  }
- 
-
-
 }
